ea20390efa3f861c12ee5d790500328b9216b7b2
Behruz0770-beh <bhrzshvktv@gmail.com>
2025-05-05 17:22:11 +0500

    Update homework1.sql

--- lesson-1/homework/homework1.sql
+++ lesson-1/homework/homework1.sql
@@ -1,1 +1,25 @@
+(data) – Bu har xil raqamlar, so‘zlar yoki faktlar bo‘lib, hali tahlil qilinmagan holatda bo‘ladi. 
 
+(database) – Bu ma’lumotlarni tartibli saqlash uchun mo‘ljallangan tizimdir. U bir nechta jadvallardan tashkil topadi.
+
+ (relational database) – Ma’lumotlarni jadvallar (tablitsalar) orqali saqlaydigan va ular orasidagi bog‘liqlikni qo‘llab-quvvatlaydigan ma’lumotlar bazasi.
+
+Jadval (table) – Bu ma’lumotlar bazasida ma’lumotlar saqlanadigan joy. U satr (qator) va ustun (kolonka)lardan tashkil topgan.
+
+
+2. SQL Server ning 5 ta asosiy xususiyatini yozing.
+Katta hajmdagi malumotlarni boshqarish imkoniyati 
+
+Yuqori xavfsizlik – Foydalanuvchilarni nazorat qilish va ma’lumotlarni himoyalash imkoniyatlari bor.
+
+SSMS orqali boshqarish – SSMS (SQL Server Management Studio) yordamida qulay interfeys bilan ishlash mumkin.
+
+Transaksiyalarni qo‘llab-quvvatlaydi – Bir nechta buyruqlarni bitta operatsiya sifatida bajarish mumkin.
+
+Zaxira nusxa olish va tiklash funksiyasi – Ma’lumotlarni yo‘qolgan holatda qayta tiklash mumkin.
+
+
+3. SQL Server ga ulanayotganda mavjud bo‘lgan autentifikatsiya (tasdiqlash) rejimlari qanday? (Kamida 2 ta yozing):
+
+Windows Authentication – Foydalanuvchi Windows tizimidagi login orqali avtomatik ulanadi.
+SQL Server Authentication – Maxsus foydalanuvchi nomi va parol orqali ulaniladi.
