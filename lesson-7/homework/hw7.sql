80eb269f4f001ce9d86ca69a407dc0563cb7edfa
Behruz0770-beh <bhrzshvktv@gmail.com>
2025-05-24 16:10:17 +0500

    Update hw7.sql

--- lesson-7/homework/hw7.sql
+++ lesson-7/homework/hw7.sql
@@ -1,1 +1,43 @@
+1. select productname,min(price) as ninimum from products
+group by productname
+
+2.select max(salary) from employees
+
+3.select count(customerid) from customers
+
+4.select category, count(category) as umumiy from products
+group by category
+
+5.select sum(saleamount) as total_saleamount from sales
+group by productid
+having productid=7
+
+6.select  avg(age) as avg_age from employees;
+
+7.select departmentname, count(departmentname) as nimadur from employees
+group by departmentname
+
+8.select category,min(price)min_price,max(price)max_price from products
+group by category
+
+9.select customerid,sum(saleamount)percustomer_totalsaleamount,count(saleid) percustomer_totalsales from sales
+group by customerid;
+
+10.select departmentname,count(departmentname) as total_employees from employees
+group by departmentname
+having count(departmentname)>5
+
+11.
+
+
+
+
+
+
+
+
+
+
+
+
 
