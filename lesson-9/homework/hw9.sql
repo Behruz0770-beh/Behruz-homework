7f68a2fed2e500a3f4f470a7bac17db297ffa78b
Behruz0770-beh <bhrzshvktv@gmail.com>
2025-05-31 18:45:15 +0500

    Update hw9.sql

--- lesson-9/homework/hw9.sql
+++ lesson-9/homework/hw9.sql
@@ -1,1 +1,12 @@
+1.SELECT products.productname, suppliers.suppliername
+FROM products 
+CROSS JOIN suppliers;
 
+2.select*from departments,employees;
+
+3.select suppliername,productname from products inner join suppliers on products.supplierid=suppliers.supplierid;
+
+4.select concat(firstname ,' ', lastname) as customer_name,orderID
+from orders inner join customers on Orders.customerId=Customers.customerid;
+
+5.
